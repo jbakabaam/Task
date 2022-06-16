9e5707ca09bb5b22fd3a763d5f0697f7874a7797
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-06-16 14:10:26 +0900

    Update columns_in_nps_tables.py

--- columns_in_nps_tables.py
+++ columns_in_nps_tables.py
@@ -1,3 +1,8 @@
+# nps_2013_t30
+# 15 columns
+names=['SPEC_ID_SNO','LN_NO','CZ_ITEM_CD','DIV_TY_CD','DIV_CD','SP1_SP2_TP_CD','FQ1_MDCT_QTY','DY1_MDCT_QTY','DY1_INJC_QTY_EXEC_FQ','TOT_INJC_DDCNT_EXEC_FQ',
+       'TOT_USE_QTY_OR_EXEC_FQ', 'UNPRC', 'AMT', 'ADDC_ADT_AMT', 'GNL_NM_CD']
+
 # nps_2013/2014/2015_ykiho
 # 13 columns
 names=['YID', 'DATE', 'SIDO_CD', 'CL_CD', 'ORG_DF', 'BED_GRADE',
