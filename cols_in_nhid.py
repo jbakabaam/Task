6762096f1198b7bfb03c513c3d38b8c0f4d988c2
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-07-01 10:49:19 +0900

    Update cols_in_nhid.py

--- cols_in_nhid.py
+++ cols_in_nhid.py
@@ -46,3 +46,42 @@ Index(['STND_Y', 'YKIHO_ID', 'YKIHO_GUBUN_CD', 'ORG_TYPE', 'YKIHO_SIDO',
       dtype='object')
 len(df_list[0].columns)
 10
+
+#
+#
+#
+
+# nhid_gj_2004
+Index(['HCHK_YEAR', 'PERSON_ID', 'YKIHO_GUBUN_CD', 'HEIGHT', 'WEIGHT',
+       'BP_HIGH', 'BP_LWST', 'BLDS', 'TOT_CHOLE', 'HMG', 'GLY_CD',
+       'OLIG_OCCU_CD', 'OLIG_PH', 'OLIG_PROTE_CD', 'SGOT_AST', 'SGPT_ALT',
+       'GAMMA_GTP', 'HCHK_PMH_CD1', 'HCHK_PMH_CD2', 'HCHK_PMH_CD3',
+       'FMLY_LIVER_DISE_PATIEN_YN', 'FMLY_HPRTS_PATIEN_YN',
+       'FMLY_APOP_PATIEN_YN', 'FMLY_HDISE_PATIEN_YN', 'FMLY_DIABML_PATIEN_YN',
+       'FMLY_CANCER_PATIEN_YN', 'SMK_STAT_TYPE_RSPS_CD', 'SMK_TERM_RSPS_CD',
+       'DSQTY_RSPS_CD', 'DRNK_HABIT_RSPS_CD', 'TM1_DRKQTY_RSPS_CD',
+       'EXERCI_FREQ_RSPS_CD'],
+      dtype='object')
+len(df_list[0].columns)
+32
+
+# nhid_jk_2004
+Index(['STND_Y', 'PERSON_ID', 'SEX', 'AGE_GROUP', 'DTH_YM', 'DTH_CODE1',
+       'DTH_CODE2', 'SIDO', 'SGG', 'IPSN_TYPE_CD', 'CTRB_PT_TYPE_CD',
+       'DFAB_GRD_CD', 'DFAB_PTN_CD', 'DFAB_REG_YM'],
+      dtype='object')
+len(df_list[0].columns)
+14
+
+# nhid_t20_2004
+Index(['PERSON_ID', 'KEY_SEQ', 'YKIHO_ID', 'RECU_FR_DT', 'FORM_CD', 'DSBJT_CD',
+       'MAIN_SICK', 'SUB_SICK', 'IN_PAT_CORS_TYPE', 'OFFC_INJ_TYPE', 'RECN',
+       'VSCN', 'FST_IN_PAT_DT', 'DMD_TRAMT', 'DMD_SBRDN_AMT', 'DMD_JBRDN_AMT',
+       'DMD_CT_TOT_AMT', 'DMD_MRI_TOT_AMT', 'EDEC_ADD_RT', 'EDEC_TRAMT',
+       'EDEC_SBRDN_AMT', 'EDEC_JBRDN_AMT', 'EDEC_CT_TOT_AMT',
+       'EDEC_MRI_TOT_AMT', 'DMD_DRG_NO', 'TOT_PRES_DD_CNT'],
+      dtype='object')
+len(df_list[0].columns)
+26
+
+# nhid_t30_2004
