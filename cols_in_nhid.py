4c541c1e054828216b022eb7c08c1712f42d9e55
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-07-01 13:02:25 +0900

    Update cols_in_nhid.py

--- cols_in_nhid.py
+++ cols_in_nhid.py
@@ -97,9 +97,44 @@ Index(['KEY_SEQ', 'SEQ_NO', 'RECU_FR_DT', 'DSBJT_CD', 'SICK_SYM'], dtype='object
 len(df_list[0].columns)
 5
 
-# nhid_t60_2014
+# nhid_t60_2004
 Index(['KEY_SEQ', 'SEQ_NO', 'RECU_FR_DT', 'DIV_TYPE_CD', 'DIV_CD', 'GNL_NM_CD',
        'DD_MQTY_FREQ', 'DD_EXEC_FREQ', 'MDCN_EXEC_FREQ', 'UN_COST', 'AMT'],
       dtype='object')
 len(df_list[0].columns)
 11
+
+# nhid_yk_2004
+Index(['STND_Y', 'YKIHO_ID', 'YKIHO_GUBUN_CD', 'ORG_TYPE', 'YKIHO_SIDO',
+       'SICKBED_CNT', 'DR_CNT', 'CT_YN', 'MRI_YN', 'PET_YN'],
+      dtype='object')
+len(df_list[0].columns)
+10
+
+#
+#
+#
+
+# nhid_gj_2005
+Index(['HCHK_YEAR', 'PERSON_ID', 'YKIHO_GUBUN_CD', 'HEIGHT', 'WEIGHT',
+       'BP_HIGH', 'BP_LWST', 'BLDS', 'TOT_CHOLE', 'HMG', 'GLY_CD',
+       'OLIG_OCCU_CD', 'OLIG_PH', 'OLIG_PROTE_CD', 'SGOT_AST', 'SGPT_ALT',
+       'GAMMA_GTP', 'HCHK_PMH_CD1', 'HCHK_PMH_CD2', 'HCHK_PMH_CD3',
+       'FMLY_LIVER_DISE_PATIEN_YN', 'FMLY_HPRTS_PATIEN_YN',
+       'FMLY_APOP_PATIEN_YN', 'FMLY_HDISE_PATIEN_YN', 'FMLY_DIABML_PATIEN_YN',
+       'FMLY_CANCER_PATIEN_YN', 'SMK_STAT_TYPE_RSPS_CD', 'SMK_TERM_RSPS_CD',
+       'DSQTY_RSPS_CD', 'DRNK_HABIT_RSPS_CD', 'TM1_DRKQTY_RSPS_CD',
+       'EXERCI_FREQ_RSPS_CD'],
+      dtype='object')
+len(df_list[0].columns)
+32
+
+# nhid_jk_2005
+Index(['STND_Y', 'PERSON_ID', 'SEX', 'AGE_GROUP', 'DTH_YM', 'DTH_CODE1',
+       'DTH_CODE2', 'SIDO', 'SGG', 'IPSN_TYPE_CD', 'CTRB_PT_TYPE_CD',
+       'DFAB_GRD_CD', 'DFAB_PTN_CD', 'DFAB_REG_YM'],
+      dtype='object')
+len(df_list[0].columns)
+14
+
+# nhid_t20_2005
