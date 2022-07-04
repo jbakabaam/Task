d9948ccccaf267b0b19e180f72b290411419cfc2
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-07-04 10:03:46 +0900

    Update archive.sql

--- archive.sql
+++ archive.sql
@@ -1,7 +1,12 @@
 # Modify Columns Type
-ALTER TABLE table_name MODIFY col_name col_type
-;
+ALTER TABLE table_name MODIFY col_name col_type;
 
 # pymysql, InternalError: 1366, 'Incorrect String Value'
 ALTER DATABASE [DB명] CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE [column명] CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+# Show Charset of Database
+SHOW CREATE DATABASE DB_NAME;
+
+# Change Charset of Database
+ALTER DATABASE DB_NAME default character set = utf8mb4;
