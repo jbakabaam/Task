85e3cd52d30e635f7190e32d4d5094d8810506b8
Jun Buhm Lee <103108988+jbakabaam@users.noreply.github.com>
2022-07-04 16:40:36 +0900

    Update cols_in_nhid.py

--- cols_in_nhid.py
+++ cols_in_nhid.py
@@ -339,3 +339,63 @@ len(df_list[0].columns)
 #
 
 # nhid_gj_2009
+Index(['HCHK_YEAR', 'PERSON_ID', 'YKIHO_GUBUN_CD', 'HEIGHT', 'WEIGHT', 'WAIST',
+       'BP_HIGH', 'BP_LWST', 'BLDS', 'TOT_CHOLE', 'TRIGLYCERIDE', 'HDL_CHOLE',
+       'LDL_CHOLE', 'HMG', 'OLIG_PROTE_CD', 'CREATININE', 'SGOT_AST',
+       'SGPT_ALT', 'GAMMA_GTP', 'HCHK_APOP_PMH_YN', 'HCHK_HDISE_PMH_YN',
+       'HCHK_HPRTS_PMH_YN', 'HCHK_DIABML_PMH_YN', 'HCHK_HPLPDM_PMH_YN',
+       'HCHK_ETCDSE_PMH_YN', 'FMLY_APOP_PATIEN_YN', 'FMLY_HDISE_PATIEN_YN',
+       'FMLY_HPRTS_PATIEN_YN', 'FMLY_DIABML_PATIEN_YN',
+       'FMLY_CANCER_PATIEN_YN', 'SMK_STAT_TYPE_RSPS_CD',
+       'PAST_SMK_TERM_RSPS_CD', 'PAST_DSQTY_RSPS_CD', 'CUR_SMK_TERM_RSPS_CD',
+       'CUR_DSQTY_RSPS_CD', 'DRNK_HABIT_RSPS_CD', 'TM1_DRKQTY_RSPS_CD',
+       'MOV20_WEK_FREQ_ID', 'MOV30_WEK_FREQ_ID', 'WLK30_WEK_FREQ_ID'],
+      dtype='object')
+len(df_list[0].columns)
+40
+
+# nhid_jk_2009
+Index(['STND_Y', 'PERSON_ID', 'SEX', 'AGE_GROUP', 'DTH_YM', 'DTH_CODE1',
+       'DTH_CODE2', 'SIDO', 'SGG', 'IPSN_TYPE_CD', 'CTRB_PT_TYPE_CD',
+       'DFAB_GRD_CD', 'DFAB_PTN_CD', 'DFAB_REG_YM'],
+      dtype='object')
+len(df_list[0].columns)
+14
+
+# nhid_t20_2009
+Index(['PERSON_ID', 'KEY_SEQ', 'YKIHO_ID', 'RECU_FR_DT', 'FORM_CD', 'DSBJT_CD',
+       'MAIN_SICK', 'SUB_SICK', 'IN_PAT_CORS_TYPE', 'OFFC_INJ_TYPE', 'RECN',
+       'VSCN', 'FST_IN_PAT_DT', 'DMD_TRAMT', 'DMD_SBRDN_AMT', 'DMD_JBRDN_AMT',
+       'DMD_CT_TOT_AMT', 'DMD_MRI_TOT_AMT', 'EDEC_ADD_RT', 'EDEC_TRAMT',
+       'EDEC_SBRDN_AMT', 'EDEC_JBRDN_AMT', 'EDEC_CT_TOT_AMT',
+       'EDEC_MRI_TOT_AMT', 'DMD_DRG_NO', 'TOT_PRES_DD_CNT'],
+      dtype='object')
+len(df_list[0].columns)
+26
+
+# nhid_t30_2009
+Index(['KEY_SEQ', 'SEQ_NO', 'RECU_FR_DT', 'CLAUSE_CD', 'ITEM_CD',
+       'DIV_TYPE_CD', 'DIV_CD', 'I_II_TYPE', 'UN_COST', 'AMT',
+       'DD_MQTY_EXEC_FREQ', 'MDCN_EXEC_FREQ', 'DD_MQTY_FREQ'],
+      dtype='object')
+len(df_list[0].columns)
+13
+
+# nhid_t40_2009
+Index(['KEY_SEQ', 'SEQ_NO', 'RECU_FR_DT', 'DSBJT_CD', 'SICK_SYM'], dtype='object')
+len(df_list[0].columns)
+5
+
+# nhid_t60_2009
+Index(['KEY_SEQ', 'SEQ_NO', 'RECU_FR_DT', 'DIV_TYPE_CD', 'DIV_CD', 'GNL_NM_CD',
+       'DD_MQTY_FREQ', 'DD_EXEC_FREQ', 'MDCN_EXEC_FREQ', 'UN_COST', 'AMT'],
+      dtype='object')
+len(df_list[0].columns)
+11
+
+# nhid_yk_2009
+Index(['STND_Y', 'YKIHO_ID', 'YKIHO_GUBUN_CD', 'ORG_TYPE', 'YKIHO_SIDO',
+       'SICKBED_CNT', 'DR_CNT', 'CT_YN', 'MRI_YN', 'PET_YN'],
+      dtype='object')
+len(df_list[0].columns)
+10
